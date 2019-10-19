fd3fe8f9650c93d18622a2c693ac3b5fc0b48a6c
Zosia <b.zocha@gmail.com>
2019-10-19 21:53:50 +0200

    Added method to create star with given number of sides.

--- src/main/java/zosia/tasks/Main.java
+++ src/main/java/zosia/tasks/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
@@ -14,7 +14,7 @@ public class Main extends Application {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
 
         launch(args);
 
@@ -23,9 +23,36 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, 800, 600, Color.YELLOW);
+
+        Polygon star = setStar(300,300, 100, 5);
+        root.getChildren().add(star);
+
+
+        Scene scene = new Scene(root, 600, 600, Color.BLACK);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private static Polygon setStar (double centerX, double centerY, double radius, int sides){
+        Polygon star = new Polygon();
+        double radiusSmall = radius*Math.sin(Math.PI/(2*sides))/Math.cos(Math.PI/sides);
+        System.out.println(radiusSmall);
+        for (int i = 0; i < sides; i++) {
+            double angle = i*2*Math.PI/sides;
+            star.getPoints().addAll(radius*Math.sin(angle)+centerX,
+                    centerY-radius*Math.cos(angle));
+            System.out.println(radius*Math.sin(angle)+centerX);
+            System.out.println(centerY-radius*Math.cos(angle));
+
+            angle += Math.PI/(sides);
+            star.getPoints().addAll(radiusSmall*Math.sin(angle)+centerX,
+                    centerY-radiusSmall*Math.cos(angle));
+
+
+
+        }
+        star.setFill(Color.BLUE);
+        return star;
     }
 
 }
